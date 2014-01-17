f8ff937d020d9b668d955e8ca099e78f803565e2
Pete Albertson <albertson.peter@gmail.com>
2014-01-16 18:20:04 -0600

    Basic user model with secure passwords

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,7 +15,7 @@ describe "Static pages" do
     it { should_not have_title('| Home') }
   end
 
-    describe "Help page" do
+  describe "Help page" do
     before { visit help_path }
 
     it { should have_content('Help') }
@@ -35,6 +35,44 @@ describe "Static pages" do
     it { should have_content('Contact') }
     it { should have_title(full_title('Contact')) }
   end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 end
